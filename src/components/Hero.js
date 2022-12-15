4e1dc7bd8a83315c00cdc711c9295a62c2f25a9e
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-15 23:42:25 +0100

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section className='Hero w-full h-screen'>
-        <div className="Titles">
-
+    <section className='Hero w-full h-screen block md:flex'>
+        <img src="money.png" className="relative flex w-4/5 center-h mt-16 sm:w-2/3 sm:left-0 sm:translate-x-0 sm:ml-4 md:w-1/2 md:mt-24 "></img>
+        <div className="titles relative w-full p-10 md:w-2/5 md:mt-56 ">
+          <h1 className="title text-4xl font-bold">Earn, Store, send money with <span className='bg-red-500 rounded-xl text-slate-100'>MonoCash</span></h1>
+          <h2 className="subtitle text-2xl text-gray-800">Most popular trading & banking platform in your hands</h2>
         </div>
+
     </section>
   )
 }
