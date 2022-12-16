9e336712f9f0176a26a73390a39f36eaae75db8d
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-16 23:12:42 +0100

    hero 2nd page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,25 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section className='Hero w-full h-screen block md:flex'>
-        <img src="money.png" className="relative flex w-4/5 center-h mt-16 sm:w-2/3 sm:left-0 sm:translate-x-0 sm:ml-4 md:w-1/2 md:mt-24 "></img>
-        <div className="titles relative w-full p-10 md:w-2/5 md:mt-56 ">
-          <h1 className="title text-4xl font-bold">Earn, Store, send money with <span className='bg-red-500 rounded-xl text-slate-100'>MonoCash</span></h1>
-          <h2 className="subtitle text-2xl text-gray-800">Most popular trading & banking platform in your hands</h2>
+    <div className="">
+      <section className='Hero w-full h-screen block md:flex'>
+          <img src="money.png" className="relative flex w-4/5 center-h mt-16 sm:w-2/3 sm:left-0 sm:translate-x-0 sm:ml-4 md:w-6/12 md:mt-24 md:mr-16"></img>
+          <div className="titles static w-full p-10 md:w-5/12 md:mt-56 ">
+            <h1 className="title text-4xl font-bold">Earn, Store, send money with <span className='bg-red-500 rounded-xl text-slate-100'>MonoCash</span></h1>
+            <h2 className="subtitle text-2xl text-gray-800">Most popular trading & banking platform in your hands</h2>
+          </div>
+      </section>
+      <section className='h-screen block bg-red-500 p-10 md:p-26'>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex justify-evenly items-center lg:w-4/5 p-4 ml-auto mr-auto mt-6'>
+          <img src="cash.png" alt="" className='w-36 sm:w-44'/>
+          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">Fast and secure online transfer</h1>
+        </div>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex mt-8 justify-evenly items-center lg:w-4/5 p-4 ml-auto mr-auto'>
+          <img src="shield.png" alt="" className='w-36 sm:w-44'/>
+          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">number 1 trusted & certified platform</h1>
         </div>
 
-    </section>
+      </section>
+    </div>
   )
 }
