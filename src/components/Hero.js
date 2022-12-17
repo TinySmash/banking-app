dc04dbc5dd0cd97b329a71a8afe2914cc6828cf6
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-17 08:41:33 +0100

    hero 2nd page done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,14 +10,22 @@ export default function Hero() {
             <h2 className="subtitle text-2xl text-gray-800">Most popular trading & banking platform in your hands</h2>
           </div>
       </section>
-      <section className='h-screen block bg-red-500 p-10 md:p-26'>
-        <div className='w-full h-min bg-slate-100 rounded-xl flex justify-evenly items-center lg:w-4/5 p-4 ml-auto mr-auto mt-6'>
+      <section className='h-auto block pb-4 bg-red-500 p-10 md:p-26 md:flex md:flex-wrap lg:pt-16 lg:pb-10'>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex justify-evenly items-center lg:w-9/2 p-4 ml-auto mr-auto mb-8'>
           <img src="cash.png" alt="" className='w-36 sm:w-44'/>
           <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">Fast and secure online transfer</h1>
         </div>
-        <div className='w-full h-min bg-slate-100 rounded-xl flex mt-8 justify-evenly items-center lg:w-4/5 p-4 ml-auto mr-auto'>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex justify-evenly items-center lg:w-9/2 p-4 ml-auto mr-auto mb-8'>
           <img src="shield.png" alt="" className='w-36 sm:w-44'/>
-          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">number 1 trusted & certified platform</h1>
+          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">First trusted & certified platform</h1>
+        </div>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex mb-8 justify-evenly items-center lg:w-9/2 p-4 ml-auto mr-auto'>
+          <img src="globe.png" alt="" className='w-36 sm:w-44'/>
+          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">Send to anywhere in less than 5 minutes</h1>
+        </div>
+        <div className='w-full h-min bg-slate-100 rounded-xl flex justify-evenly items-center lg:w-9/2 p-4 ml-auto mr-auto mb-8'>
+          <img src="tax.png" alt="" className='w-36 sm:w-44'/>
+          <h1 className=" font-bold text-2xl text-red-500 sm:text-4xl">100% tax free service (We don't take fees)</h1>
         </div>
 
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '9/2': '45%'
+      }
+    },
   },
   plugins: [],
 }
