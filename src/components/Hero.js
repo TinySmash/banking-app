fb7247b243f0e669fe8715da3f1e49dc3ac9ead2
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-19 22:27:47 +0100

    mobile menu (still working on it)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="">
       <section className='Hero w-full h-screen block md:flex'>
-          <img src="money.png" className="relative flex w-4/5 center-h mt-16 sm:w-2/3 sm:left-0 sm:translate-x-0 sm:ml-4 md:w-6/12 md:mt-24 md:mr-16"></img>
+          <img src="money.png" className="relative flex w-4/5 center-h mt-16 sm:w-2/3 sm:left-0 sm:translate-x-0 sm:ml-4 md:w-6/12 md:mt-24 md:mr-16 lg:w-5/12"></img>
           <div className="titles static w-full p-10 md:w-5/12 md:mt-56 ">
             <h1 className="title text-4xl font-bold">Earn, Store, send money with <span className='bg-red-500 rounded-xl text-slate-100'>MonoCash</span></h1>
             <h2 className="subtitle text-2xl text-gray-800">Most popular trading & banking platform in your hands</h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 export default function Navbar() {
+
+  let menuOpened = false;
+  function openMenu() {
+    let menu = document.querySelector('.burger-opened-menu');
+    if(menuOpened == false) {
+      menu.classList.remove('hidden');
+      menu.classList.add('block');
+      menu.classList.add('burger-opened-menu')
+      
+      menuOpened = true
+    }else if(menuOpened == true) {
+      menu.classList.add('hidden')
+      menu.classList.remove('block')
+      menuOpened = false
+    }
+  }
+
   return (
     <header className='flex fixed h-12 w-full p-8 sm:pr-16 bg-red-500 justify-between items-center z-50'>
         <h1 className="title text-3xl font-bold text-slate-100">MonoCash</h1>
@@ -11,9 +28,14 @@ export default function Navbar() {
                 <li className='no-underline text-slate-100 hover:text-slate-400'><a href="">Wallet</a></li>
             </ul>
         </nav>
-        <button className='flex sm:hidden w-10 h-10 border-2 border-slate-100 border-radius-5 rounded items-center justify-center'>
+        <button className='flex sm:hidden w-10 h-10 border-2 border-slate-100 border-radius-5 rounded items-center justify-center' onClick={openMenu}>
             <div className="burger-menu w-8 h-1 before:w-8 before:h-1 before:border-slate-100 before:rounded-md after:rounded-md after:border-slate-100 after:w-8 after:h-1 border-2 border-slate-100 rounded-md"></div>
         </button>
+        <ul className='hidden sm:hidden absolute right-0 top-16 w-28 h-28 border-2 border-red-500 rounded-b-md bg-red-500'>
+          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Home</a></li>
+          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Profile</a></li>
+          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Wallet</a></li>
+        </ul>
     </header>
   )
 }
