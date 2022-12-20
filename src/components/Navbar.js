7d376e7626bf60ae39b59fc8e421b60d3f47686c
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-20 15:49:01 +0100

    mobile menu done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,20 @@ export default function Navbar() {
 
   let menuOpened = false;
   function openMenu() {
-    let menu = document.querySelector('.burger-opened-menu');
+    const menu = document.querySelector('.burger-options-menu');
+    const menuList = document.querySelector('.menu-list');
     if(menuOpened == false) {
-      menu.classList.remove('hidden');
+      menu.classList.remove('burger-closed-menu');
       menu.classList.add('block');
       menu.classList.add('burger-opened-menu')
-      
+      menuList.classList.remove('menu-list-closed')
+      menuList.classList.add('menu-list-opened')
       menuOpened = true
     }else if(menuOpened == true) {
-      menu.classList.add('hidden')
-      menu.classList.remove('block')
+      menu.classList.add('burger-closed-menu')
+      menu.classList.remove('burger-opened-menu')
+      menuList.classList.add('menu-list-closed')
+      menuList.classList.remove('menu-list-opened')
       menuOpened = false
     }
   }
@@ -31,11 +35,13 @@ export default function Navbar() {
         <button className='flex sm:hidden w-10 h-10 border-2 border-slate-100 border-radius-5 rounded items-center justify-center' onClick={openMenu}>
             <div className="burger-menu w-8 h-1 before:w-8 before:h-1 before:border-slate-100 before:rounded-md after:rounded-md after:border-slate-100 after:w-8 after:h-1 border-2 border-slate-100 rounded-md"></div>
         </button>
-        <ul className='hidden sm:hidden absolute right-0 top-16 w-28 h-28 border-2 border-red-500 rounded-b-md bg-red-500'>
-          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Home</a></li>
-          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Profile</a></li>
-          <li className='no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Wallet</a></li>
-        </ul>
+        <div className='burger-options-menu burger-closed-menu sm:hidden absolute right-0 top-16 w-28 h-28 border-2 border-red-500 rounded-b-md bg-red-500'>
+          <ul className='menu-list menu-list-closed'>
+            <li className='menu-options no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Home</a></li>
+            <li className='menu-options no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2'><a href="">Profile</a></li>
+            <li className='menu-options no-underline text-slate-100 hover:text-slate-400 ml-7 mt-2 '><a href="">Wallet</a></li>
+          </ul>
+        </div>
     </header>
   )
 }
