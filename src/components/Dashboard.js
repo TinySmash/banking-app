71630be059aa388ebc63644822ba8c80c5297ade
Islam Elgueniari <islam.gueniari@gmail.com>
2023-02-27 01:22:32 +0100

    grid setup

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,13 +27,13 @@ class Dashboard extends Component {
       <div className='w-full h-auto bg-slate-100 p-4'>
 
         { (user?.loginInfo?.isLoggedIn == true) ? <div className='dashboard'>
-          <div className='w-full flex h-auto mt-14'>
+          <div className='w-full grid gap-4 grid-cols-2 grid-rows-2 h-auto mt-14'>
 
-            <div className='border-2 border-slate-500 relative block text-center w-9/2 md:w-1/5 md:min-w-72 h-56 md:h-72 mr-2 mx-auto rounded-md'>
+            <div className='border-2 border-slate-500 block text-center rounded-md'>
               <h1 className='balance text-2xl font-medium w-auto mx-auto mt-9 md:mt-20 md:font-semibold'>Personal balance</h1>
               <h1 className="balance text-3xl mt-6">0 $</h1>
             </div>
-            <div className='border-2 border-slate-500 relative flex justify-start items-start w-9/2 md:w-4/5 h-56 md:h-72 mx-auto rounded-md p-1'>
+            <div className='border-2 border-slate-500 flex justify-start items-start rounded-md p-1 py-20'>
               <div className='all-acc block relative h-full w-full md:w-1/3 p-2'>
                 <label className='text-lg absolute sm:text-xl border-b-2 border-slate-500 cursor-pointer'>Transactions history</label>
                 
@@ -54,23 +54,23 @@ class Dashboard extends Component {
                 
               </div>
             </div>
-
+            <div className='flex p-2 py-16 col-start-1 col-end-3 border-2 border-slate-500 justify-evenly rounded-md'>
+              <div className='block relative w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
+                <ProgressBar percentage={15} count={1}/>
+                <h1 className='absolute text-md md:text-lg text-bold bottom-[-45px] left-0 right-0 mx-auto'>value progress</h1>
+              </div>
+              <div className='w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
+                <ProgressBar percentage={65} count={2}/>
+              </div>
+              <div className='hidden sm:flex sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
+                <ProgressBar percentage={85} count={3}/>
+              </div>
+              <div className='hidden md:flex md:w-32 md:h-32 lg:w-36 lg:h-36'>
+                <ProgressBar percentage={25} count={4}/>
+              </div>
+            </div>
           </div>
-          <div className='mt-2 p-2 border-2 border-slate-500 relative flex justify-evenly w-full h-56 md:h-64 mx-auto rounded-md'>
-            <div className='block relative w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
-              <ProgressBar percentage={15} count={1}/>
-              <h1 className='absolute text-md md:text-lg text-bold bottom-[-45px] left-0 right-0 mx-auto'>value progress</h1>
-            </div>
-            <div className='w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
-              <ProgressBar percentage={65} count={2}/>
-            </div>
-            <div className='hidden sm:flex sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36'>
-              <ProgressBar percentage={85} count={3}/>
-            </div>
-            <div className='hidden md:flex md:w-32 md:h-32 lg:w-36 lg:h-36'>
-              <ProgressBar percentage={25} count={4}/>
-            </div>
-          </div>
+          
 
         </div> : 
 
